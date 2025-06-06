b7add37aa4767ff89aa4e44ae7202440926478a6
FranciscoGarc <20220326@ricaldone.edu.sv>
2025-06-06 10:01:21 -0600

    feat: localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar.jsx";
 import TaskList from "./components/TaskList.jsx";
 import AddTaskModal from "./components/AddTaskModal.jsx";
 import EditTaskModal from "./components/EditTaskModal.jsx";
 import './App.css';
 
+/*
 const initialTasks = [
     { id:1, title: "Ir de compras", dueDate: "Vence hoy", completed: false },
     { id:2, title: "Regreso de libro en biblioteca", dueDate: "Vence mañana", completed: false },
@@ -12,10 +13,21 @@ const initialTasks = [
     { id:4, title: "Plan de vacacion", dueDate: "Vence en 4 días", completed: false },
     { id:5, title: "Renovar suscripcion", dueDate: "Vence mañana", completed: false },
 ];
+*/
 
 function App() {
     // State to manage tasks
-    const [tasks, setTasks] = useState(initialTasks);
+    const [tasks, setTasks] = useState(() => {
+        const savedTasks = localStorage.getItem('tasks_v1');
+        if (savedTasks) {
+            return JSON.parse(savedTasks);
+        }else {
+            return [];
+        }
+    });
+    useEffect(() => {
+        localStorage.setItem('tasks_v1', JSON.stringify(tasks));
+    }, [tasks]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [editingTask, setEditingTask] = useState(null);
     // Handlers for modal and task operations
